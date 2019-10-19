1febf09b795949adb4012bb70628b9e71022bb41
shatakshimohan <54231062+shatakshimohan@users.noreply.github.com>
2019-10-20 00:51:39 +0530

    simple calculator
    simple calculator that performs simple mathematical functions.

---
+++ Calculator/simpcalc.py
@@ -0,0 +1,31 @@
+# for adding
+def add(x, y):
+   return x + y
+# for subtracting
+def subtract(x, y):
+   return x - y
+# for multiplying
+def multiply(x, y):
+   return x * y
+# for dividing
+def divide(x, y):
+   return x / y
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+# input from user
+choice = input("Enter choice(1/2/3/4):")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+else:
+   print("Invalid input")
